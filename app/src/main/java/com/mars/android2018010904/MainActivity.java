ec72ec106b6f45deba07851b1ac76afc276ce6cd
MarsChang <mars09250148@yahoo.com.tw>
2018-01-09 13:28:30 +0800

    customized menu

--- app/src/main/java/com/mars/android2018010904/MainActivity.java
+++ app/src/main/java/com/mars/android2018010904/MainActivity.java
@@ -16,9 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+      /*
+      menu可以用圖像介面來自定
+      res==>產生menu資料夾==>menu檔案
+       */
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+
         ////參數1:群組id, 參數2:itemId, 參數3:item順序, 參數4:item名稱
-        menu.add(0,1,1,"ADD");
-        menu.add(0,2,0,"edit");
+        //menu.add(0,1,1,"ADD");
+        //menu.add(0,2,0,"edit");
         //menu.add("ADD");//比較簡單的寫法
         return super.onCreateOptionsMenu(menu);
     }
@@ -28,12 +34,13 @@ public class MainActivity extends AppCompatActivity {
         //使用switch來抓是哪一個
         switch (item.getItemId())//找出item的id
         {
-            case 1:
-                Toast.makeText(MainActivity.this,"按下ADD",Toast.LENGTH_SHORT).show();
+            case R.id.menu_add:
+                Toast.makeText(MainActivity.this,"按下新增",Toast.LENGTH_SHORT).show();
                 break;
-            case 2:
-                Toast.makeText(MainActivity.this,"按下edit",Toast.LENGTH_SHORT).show();
+            case R.id.menu_del:
+                Toast.makeText(MainActivity.this,"按下刪除",Toast.LENGTH_SHORT).show();
                 break;
+
 
         }
 
